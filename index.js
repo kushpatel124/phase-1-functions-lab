4954774ecb2526b296701f5b4ee267e4d5282864
Kush <kushpatel124@gmail.com>
2022-12-05 23:32:33 -0500

    commit tree bfb3abdc4ed133f0a9c894ec56296a72e7431a87

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(42 - someValue);
+}
+
+
+function distanceFromHqInFeet(someValue) {
+  distanceFromHqInBlocks(someValue);
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let result;
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    result = 0;
+  } else if (distance > 400 && distance < 2000) {
+    result = (distance - 400) * 0.02;
+  } else if (distance >= 2000 && distance < 2500) {
+    result = 25;
+  } else {
+    result = 'cannot travel that far';
+  }
+  return result;
+}
